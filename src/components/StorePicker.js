e0b299564ef3b5f70bc3fd9db8f5a579d381dcc1
Kyron Stubbs <B@Kyrons-MBP.lan>
2017-05-10 17:08:31 -0300

    added unstyled form

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 class StorePicker extends React.Component {
 
-	render(){
-		return <p>Sup</p>;
-	}
+    render() {
+        return (
+			<form className="store-selector">
+				<h2>Please Enter A Store</h2>
+				<input type="text" required placeholder="Store Name"/>
+				<button type="submit">Visit Store -></button>
+			</form>
+        )
+    }
 }
 
 export default StorePicker;
