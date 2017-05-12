5f8b0a3a1dfdccd14f4d5701b14f6416462a7ea5
Kyron Stubbs <B@Kyrons-MacBook-Pro.local>
2017-05-12 13:01:45 -0300

    using events and binding to get input value

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
 
+
+	goToStore(event){
+
+		event.preventDefault();
+		
+		//Grab text box value
+		console.log(this.storeInput.value);	
+
+
+		//Update Url and Redirect
+
+
+	}
+
+
     render() {
         return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore.bind(this)}>
 				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder="Store Name"/>
+				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
 				<button type="submit">Visit Store -></button>
 			</form>
         )
