43cf936f53ce47d2749f8354659dfa67ec700acd
Kyron Stubbs <B@Kyrons-MacBook-Pro.local>
2017-05-12 15:23:47 -0300

    firebase config

---
+++ src/Base.js
@@ -0,0 +1,11 @@
+import Rebase from 're-base';
+
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyDTBp_DNbByRH6fIGmEjlX77Gc3RnZCFic",
+    authDomain: "catch-of-the-day-13cc0.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-13cc0.firebaseio.com",
+})
+
+
+export default base;
